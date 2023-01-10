422ac2bd68b913ec0fd16ea88194b6acab9f471d
kaneki77 <pedrowayne1@gmail.com>
2023-01-10 10:58:51 -0300

    js

---
+++ javascript/02-objetos/objetos.js
@@ -0,0 +1,8 @@
+//Tudo em javaScript é um objeto
+
+var person = {
+    name: 'Pedro',
+    age: 24,
+    sex : 'male'
+};
+console.log(person)
---
+++ javascript/03-arrays/arrays.js
@@ -0,0 +1,16 @@
+var person1 = {
+    name:'pedro',
+    age: 11,
+}
+var person2 ={
+    name:'maria',
+    age:22
+}
+var person3 = {
+    name: 'jose',
+    age:56
+}
+var list = [person1,person2,person3]
+for (person of list){
+    console.log(person);
+}
